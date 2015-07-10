cf035b29b12e6c6f609aa1ee6d3105f28e8f8079
admin <admin@admindeMac-mini.local>
2015-07-10 14:34:04 +0800

    Initial Commit

---
+++ HXFStarRatingControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HXFStarRatingControl
+//
+//  Created by admin on 15/7/10.
+//  Copyright (c) 2015年 redoor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HXFStarRatingControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HXFStarRatingControl
+//
+//  Created by admin on 15/7/10.
+//  Copyright (c) 2015年 redoor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
